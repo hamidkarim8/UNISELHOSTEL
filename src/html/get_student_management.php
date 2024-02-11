7e3111e11bf6c2a735c01dfc2920876bfad7413b
khalid <muhdkhalid0533@gmail.com>
2024-02-11 16:00:18 +0800

    fix student not in bookinglist

--- src/html/get_student_management.php
+++ src/html/get_student_management.php
@@ -3,15 +3,22 @@ include_once 'db_connection.php';
 
 $students = array();
 
-$sql = "SELECT student.fullname, student.studentID, hostel.block, hostel.floor, hostel.unit, hostel.roomNumber, student.status, student.blacklist FROM student JOIN bookingList ON student.studentID = bookingList.studentID JOIN hostel ON hostel.hostelID = bookingList.hostelID WHERE student.studentID = bookingList.studentID";
+$sql = "SELECT student.fullname, student.studentID, hostel.block, hostel.floor, hostel.unit, hostel.roomNumber, student.status, student.blacklist 
+        FROM student 
+        LEFT JOIN bookingList ON student.studentID = bookingList.studentID 
+        LEFT JOIN hostel ON hostel.hostelID = bookingList.hostelID";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         // Concatenate block, floor, unit, and roomNumber into a single string
-        $room = $row['block'][6] . '-' . $row['floor'][0] . $row['floor'][6] . '-' . $row['unit'][0] . $row['unit'][5] . '-' . $row['roomNumber'][0] . $row['roomNumber'][5];
-        $row['room'] = $room;
+        if (!empty($row['block']) && !empty($row['floor']) && !empty($row['unit']) && !empty($row['roomNumber'])) {
+            $room = $row['block'][6] . '-' . $row['floor'][0] . $row['floor'][6] . '-' . $row['unit'][0] . $row['unit'][5] . '-' . $row['roomNumber'][0] . $row['roomNumber'][5];
+            $row['room'] = $room;
+        } else {
+            $row['room'] = '<span style="color:red;">No Booking Record</span>';
+        }
         $students[] = $row;
     }
     // Output JSON response
