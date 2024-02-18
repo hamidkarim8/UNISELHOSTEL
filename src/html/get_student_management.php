cbceadded820947ed48c1f87ba0e445e78d16271
hamidkarim8 <hamidkarim2002@gmail.com>
2024-02-19 02:53:03 +0800

    fix duplicate student list

--- src/html/get_student_management.php
+++ src/html/get_student_management.php
@@ -3,20 +3,47 @@ include_once 'db_connection.php';
 
 $students = array();
 
-$sql = "SELECT student.fullname, student.studentID, hostel.block, hostel.floor, hostel.unit, hostel.roomNumber, student.status, student.blacklist 
-        FROM student 
-        LEFT JOIN bookingList ON student.studentID = bookingList.studentID 
-        LEFT JOIN hostel ON hostel.hostelID = bookingList.hostelID";
+$sql = "SELECT 
+student.fullname, 
+student.studentID, 
+hostel.block, 
+hostel.floor, 
+hostel.unit, 
+hostel.roomNumber, 
+student.status, 
+student.blacklist, 
+IFNULL(bookinglist.status, 'No Booking Record') AS bookingStatus 
+FROM 
+student 
+LEFT JOIN 
+bookingList ON student.studentID = bookingList.studentID 
+LEFT JOIN 
+hostel ON hostel.hostelID = bookingList.hostelID
+WHERE 
+bookingList.bookingID IN (
+    SELECT MAX(bookingID) 
+    FROM bookingList 
+    GROUP BY studentID
+) OR bookingList.bookingID IS NULL;
+";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         // Concatenate block, floor, unit, and roomNumber into a single string
-        if (!empty($row['block']) && !empty($row['floor']) && !empty($row['unit']) && !empty($row['roomNumber'])) {
+        if (!empty($row['block']) && !empty($row['floor']) && !empty($row['unit']) && !empty($row['roomNumber']) && $row['bookingStatus']=='APPROVED') {
             $room = $row['block'][6] . '-' . $row['floor'][0] . $row['floor'][6] . '-' . $row['unit'][0] . $row['unit'][5] . '-' . $row['roomNumber'][0] . $row['roomNumber'][5];
-            $row['room'] = $room;
-        } else {
+            $row['room'] = '<span style="color:green;">' . $room . '<br>(APPROVED)</span>';
+        }elseif (!empty($row['block']) && !empty($row['floor']) && !empty($row['unit']) && !empty($row['roomNumber']) && $row['bookingStatus']=='REJECTED') {
+            $room = $row['block'][6] . '-' . $row['floor'][0] . $row['floor'][6] . '-' . $row['unit'][0] . $row['unit'][5] . '-' . $row['roomNumber'][0] . $row['roomNumber'][5];
+            $row['room'] = '<span style="color:red;">' . $room . '<br>(REJECTED)</span>';
+        }
+        elseif (!empty($row['block']) && !empty($row['floor']) && !empty($row['unit']) && !empty($row['roomNumber']) && $row['bookingStatus']=='PENDING') {
+            $room = $row['block'][6] . '-' . $row['floor'][0] . $row['floor'][6] . '-' . $row['unit'][0] . $row['unit'][5] . '-' . $row['roomNumber'][0] . $row['roomNumber'][5];
+            $row['room'] = '<span">' . $room . '<br>(PENDING)</span>';
+        }
+         else {
             $row['room'] = '<span style="color:red;">No Booking Record</span>';
         }
         $students[] = $row;
